e89a928658d6fa04987b2a53606e129da7999847
ctx-devansh <devansh.the.photofreak@gmail.com>
2024-03-23 23:32:33 -0500

    Added the OG dotted border in Audin page

--- studybuddy/src/Audin.js
+++ studybuddy/src/Audin.js
@@ -5,6 +5,7 @@ import Button from './Button';
 function Audin() {
   return (
     <div className="Audin">
+      <div className="dottedBorder"></div>
       <div className="dragArea">
         <p className="text">
           Click anywhere to choose a file.
